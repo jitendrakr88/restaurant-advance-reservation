32ad4a5f009905ca70b975ee714e959ee250fae7
Jitendra Kumar <jitendra.iitp@gmail.com>
2020-06-07 17:02:50 +0530

    [WIP]

--- src/booking/reservation/admin.py
+++ src/booking/reservation/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(User)
+admin.site.register(Restaurant)
+admin.site.register(MenuItem)
+admin.site.register(Table)
+admin.site.register(Reservation)
+admin.site.register(ReservedTable)
--- src/booking/reservation/models.py
+++ src/booking/reservation/models.py
@@ -2,12 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class User(models.Model):
+    """
+    """
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, blank=True)
+    phone = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f'{self.name}, {self.phone}'
+
 class Restaurant(models.Model):
     """Restaurant table for holding up various restaurants information in the city"""
+    slug = models.SlugField(max_length=255)
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     opens_at = models.TimeField(null=True)
     closes_at = models.TimeField(null=True)
+    tables = models.PositiveIntegerField()
 
     address = models.CharField(max_length=200, null=True, blank=True)
     city = models.CharField(max_length=50)
@@ -15,13 +27,23 @@ class Restaurant(models.Model):
     zipcode = models.CharField(max_length=10)
     country = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f'{self.name}, {self.city}'
+
 class MenuItem(models.Model):
     """Menu items for the restaurants"""
     # TODO Create a bitmap for managing the menu items category.
+    slug = models.SlugField(max_length=255)
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, blank=True, null=True)
     price = models.FloatField(default=0)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('slug', 'restaurant')
+
+    def __str__(self):
+        return f'{self.name} at {self.restaurant.name} is priced at {self.price}'
 
 class Table(models.Model):
     """Tables for the restaurants"""
@@ -32,11 +54,14 @@ class Table(models.Model):
 
 class Reservation(models.Model):
     """Manage all the reservations"""
-
+    person = models.ForeignKey(User, on_delete=models.CASCADE)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     starts = models.DateTimeField()
     ends = models.DateTimeField()
     number_of_people = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.person.name}, Reservation for {self.number_of_people} people, From {self.starts} to {self.ends}'
 
 class ReservedTable(models.Model):
     """records for reseved tables"""
