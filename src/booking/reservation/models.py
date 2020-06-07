70a570a3911f3cc698023bc4d430fa97dbf03bb2
Jitendra Kumar <jitendra.iitp@gmail.com>
2020-06-07 14:29:36 +0530

    Added menu items and tables model

--- src/booking/reservation/models.py
+++ src/booking/reservation/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Restaurant(models.Model):
@@ -14,4 +14,19 @@ class Restaurant(models.Model):
     state = models.CharField(max_length=50)
     zipcode = models.CharField(max_length=10)
     country = models.CharField(max_length=50)
+
+class MenuItem(models.Model):
+    """Menu items for the restaurants"""
+    # TODO Create a bitmap for managing the menu items category.
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    price = models.FloatField(default=0)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+class Table(models.Model):
+    """Tables for the restaurants"""
+    number = models.PositiveIntegerField()
+    capacity = models.PositiveIntegerField()
+    ocupied = models.BooleanField(default=False)
+    restaurant = models.ForeignKey("Restaurant", on_delete=models.CASCADE)
 
